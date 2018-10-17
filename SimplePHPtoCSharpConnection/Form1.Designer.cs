e0fd07862051131a9e3d207db2672f63efe6ca8f
asandru <artister.hd@gmail.com>
2018-10-17 23:11:11 +0200

    update
    Signed-off-by: asandru <artister.hd@gmail.com>

--- SimplePHPtoCSharpConnection/Form1.Designer.cs
+++ SimplePHPtoCSharpConnection/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(174, 59);
+            this.textBox1.Location = new System.Drawing.Point(180, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -44,7 +46,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 62);
+            this.label1.Location = new System.Drawing.Point(119, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
@@ -54,33 +56,52 @@
             // 
             this.label2.AutoEllipsis = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.label2.Location = new System.Drawing.Point(0, 96);
+            this.label2.Location = new System.Drawing.Point(0, 100);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(495, 73);
+            this.label2.Size = new System.Drawing.Size(575, 73);
             this.label2.TabIndex = 3;
             this.label2.Text = "Result";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 57);
+            this.button1.Location = new System.Drawing.Point(206, 54);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(172, 23);
-            this.button1.TabIndex = 4;
+            this.button1.TabIndex = 2;
             this.button1.Text = "Get Result";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(353, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 169);
+            this.ClientSize = new System.Drawing.Size(575, 173);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -93,6 +114,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- SimplePHPtoCSharpConnection/Form1.cs
+++ SimplePHPtoCSharpConnection/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SimplePHPtoCSharpConnection
@@ -18,10 +11,13 @@ namespace SimplePHPtoCSharpConnection
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
-            string Response = new WebClient().DownloadString($"http://scenegaming.com/appconnect/index.php?username={textBox1.Text}");
-            label2.Text = Response.ToLower();
+            using (WebClient client = new WebClient())
+            {
+                label2.Text = client.DownloadString("http://scenegaming.com/appconnect/index.php?" +
+                    $"username={textBox1.Text}" + "&" + $"password={textBox2.Text}");
+            }
         }
     }
 }
